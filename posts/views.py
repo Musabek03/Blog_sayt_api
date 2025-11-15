46078abb3ac494465f8c106f72a8e89a3953e9bd
Musabek <makhambetjaliev@gmail.com>
2025-11-15 21:50:55 +0500

    permissions

--- posts/views.py
+++ posts/views.py
@@ -1,24 +1,33 @@
 from .models import Post,Category,Tag,Comment
 from .serializers import PostSerializer,CategorySerializer, TagSerializer,CommentSerializer
-from rest_framework import viewsets,mixins
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly 
+from rest_framework import permissions
+from .permissions import IsAuthorOrReadOnly, IsAdminOrReadOnly, IsAdminOrCreateOnlyOrReadOnly
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author = self.request.user)
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-
+    permission_classes = [IsAdminOrReadOnly]
+    permission_classes = [IsAdminOrCreateOnlyOrReadOnly]
 
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
-
+    permission_classes = [IsAdminOrReadOnly]
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAdminUser]
