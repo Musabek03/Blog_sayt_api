3d5067e003775718dfd8c1a2aaddc3e20f1be577
Musabek <makhambetjaliev@gmail.com>
2025-12-10 16:29:08 +0500

    nginx

--- config/settings.py
+++ config/settings.py
@@ -119,8 +119,11 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Password validation
---
+++ esap.py
@@ -0,0 +1,19 @@
+#N1
+
+# a = int(input("Bir san kiritin:"))
+# san=1
+# for i in range(1,11):
+#     san = i*a
+#     print(san)
+
+
+
+###### N2
+
+# a=0
+# san =0
+# while a<+100:
+#     a=a+1
+#     san =san +a
+
+# print(san)
